fdaaa06b9212219a098b473ae3ca566e006db22a
CCCCChloe <tpanchloe@gmail.com>
2019-10-06 00:15:41 -0500

    update

--- static/script.js
+++ static/script.js
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+alert("aha");
 'use strict';
 
 // [START gae_python37_auth_javascript]
